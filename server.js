21a9f2b341e106cfe9bb5e8b3faa5520051f2b05
SofiShaikh <sofishaikh8786@gmail.com>
2025-01-06 15:32:54 +0530

    Commiting new changes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb =require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
+
 connectDb();
 const app = express();
-
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
